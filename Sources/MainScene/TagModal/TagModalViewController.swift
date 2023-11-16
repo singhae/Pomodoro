26f69fd6818c16f638092f53c59411a186b13ee1
Seunghae Son <thsgo1357@naver.com>
2023-11-17 02:23:10 +0900

    [Feat] Tag Modal 기능추가 (PanModal)

---
+++ Sources/MainScene/TagModal/TagModalViewController.swift
@@ -0,0 +1,68 @@
+//
+//  TagModalViewController.swift
+//  Pomodoro
+//
+//  Created by SonSinghae on 2023/11/17.
+//  Copyright © 2023 io.hgu. All rights reserved.
+//
+
+final class TagModalViewController: UIViewController {
+    private let label: UILabel = {
+        let label = UILabel()
+        label.text = "태그"
+        label.font = UIFont.systemFont(ofSize: 40)
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    private let tableView: UITableView = {
+        let view = UITableView()
+        view.allowsSelection = false
+        view.backgroundColor = .clear
+        view.separatorStyle = .none
+        view.contentInset = .zero
+        view.estimatedRowHeight = 34
+        view.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    var scrollView: UIScrollView {
+        tableView
+    }
+    
+    private let items = (0...10).map(String.init)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        tableView.dataSource = self
+        
+        view.addSubview(label)
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            label.topAnchor.constraint(equalTo: view.topAnchor),
+        ])
+        
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.topAnchor.constraint(equalTo: label.bottomAnchor),
+        ])
+    }
+}
+
+extension ModalViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        items.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = items[indexPath.row]
+        return cell ?? UITableViewCell()
+    }
+}
