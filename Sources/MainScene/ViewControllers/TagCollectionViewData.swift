7e971e253b3ee9ea770c815220d12bdee9f2aa8e
Seunghae Son <thsgo1357@naver.com>
2024-01-10 00:01:16 +0900

    [fix] fix force unwrapping

--- Sources/MainScene/ViewControllers/TagCollectionViewData.swift
+++ Sources/MainScene/ViewControllers/TagCollectionViewData.swift
@@ -10,6 +10,7 @@ import Foundation
 
 //FIXME
 struct TagCollectionViewData {
+    //MARK: - FIXME
     static let data = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10","11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
 
 }
