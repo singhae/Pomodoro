d0bb824177d15bb39a1acc7cdd824952f309613b
jinsejin <j03298969@gmail.com>
2024-01-30 21:06:03 +0900

    [Fix] TimerCollectionViewCell.swift 추가

--- Sources/MainScene/Views/TimerCollectionViewCell.swift
+++ Sources/MainScene/Views/TimerCollectionViewCell.swift
@@ -6,11 +6,20 @@
 //  Copyright © 2023 io.hgu. All rights reserved.
 //
 
+<<<<<<< HEAD
 import SnapKit
 import Then
 import UIKit
 
 class TimerCollectionViewCell: UICollectionViewCell {
+=======
+import UIKit
+import Then
+import SnapKit
+
+class TimerCollectionViewCell: UICollectionViewCell {
+    
+>>>>>>> 8412668 ([Fix] TimerCollectionViewCell.swift 추가)
     let timeLabel = UILabel().then {
         $0.textColor = .black
         $0.textAlignment = .center
@@ -22,11 +31,16 @@ class TimerCollectionViewCell: UICollectionViewCell {
         $0.image = UIImage(systemName: "arrowtriangle.up.fill")
         $0.tintColor = .black
     }
+<<<<<<< HEAD
 
+=======
+    
+>>>>>>> 8412668 ([Fix] TimerCollectionViewCell.swift 추가)
     private let timeCircleView = UIView().then {
         $0.backgroundColor = .gray
         $0.layer.masksToBounds = true
     }
+<<<<<<< HEAD
 
     var isSelectedTime: Bool = false {
         didSet {
@@ -45,26 +59,61 @@ class TimerCollectionViewCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
 
+=======
+       
+    public var isSelectedTime : Bool = false {
+       didSet {
+           timeSelectionImage.isHidden = !isSelectedTime
+           timeCircleView.backgroundColor = isSelectedTime ? .black : .systemGray
+       }
+    }
+
+    override init(frame: CGRect) {
+       super.init(frame: frame)
+       setupViews()
+    }
+
+    required init?(coder: NSCoder) {
+       fatalError("init(coder:) has not been implemented")
+    }
+    
+>>>>>>> 8412668 ([Fix] TimerCollectionViewCell.swift 추가)
     override func layoutSubviews() {
         super.layoutSubviews()
         timeCircleView.layer.cornerRadius = timeCircleView.bounds.width / 2
     }
+<<<<<<< HEAD
 
+=======
+    
+>>>>>>> 8412668 ([Fix] TimerCollectionViewCell.swift 추가)
     private func setupViews() {
         addSubview(timeLabel)
         addSubview(timeSelectionImage)
         addSubview(timeCircleView)
+<<<<<<< HEAD
 
+=======
+        
+>>>>>>> 8412668 ([Fix] TimerCollectionViewCell.swift 추가)
         timeLabel.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.bottom.equalTo(self.snp.top).offset(9.0)
         }
+<<<<<<< HEAD
 
+=======
+        
+>>>>>>> 8412668 ([Fix] TimerCollectionViewCell.swift 추가)
         timeSelectionImage.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.bottom.equalTo(self.snp.bottom).offset(15.0)
         }
+<<<<<<< HEAD
 
+=======
+        
+>>>>>>> 8412668 ([Fix] TimerCollectionViewCell.swift 추가)
         timeCircleView.snp.makeConstraints { make in
             make.centerX.centerY.equalToSuperview()
             make.width.height.equalTo(self.snp.width).multipliedBy(0.7)
