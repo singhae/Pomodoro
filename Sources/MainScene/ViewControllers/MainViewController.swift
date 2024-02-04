6b8c4a0ad5655e5f98acc1a8b8ba26a597e91ffd
jinsejin <j03298969@gmail.com>
2024-02-04 18:20:37 +0900

    [Chore] Pull이후 간단한 오류 수정

--- Sources/MainScene/ViewControllers/MainViewController.swift
+++ Sources/MainScene/ViewControllers/MainViewController.swift
@@ -13,7 +13,6 @@ import UIKit
 
 final class MainViewController: UIViewController {
     private var timer: Timer?
-    private var stopLongPress: UILongPressGestureRecognizer!
     private var notificationId: String?
     private var currentTime = 0
     private var maxTime = 0
@@ -22,7 +21,7 @@ final class MainViewController: UIViewController {
         $0.textAlignment = .center
         $0.font = UIFont.systemFont(ofSize: 60, weight: .heavy)
     }
-    
+
     private let longPressGuideLabel = UILabel().then {
         $0.text = "길게 클릭해서 타이머를 정지할 수 있어요"
         $0.textAlignment = .center
@@ -31,23 +30,6 @@ final class MainViewController: UIViewController {
         $0.isHidden = true
     }
 
-    private lazy var countButton = UIButton(type: .roundedRect).then {
-        $0.setTitle("카운트 시작", for: .normal)
-        $0.addTarget(self, action: #selector(startTimer), for: .touchUpInside)
-    }
-    
-    private lazy var timeButton = UIButton(type: .roundedRect).then {
-        $0.setTitle("시간 설정", for: .normal)
-        $0.addTarget(self, action: #selector(timeSetting), for: .touchUpInside)
-    }
-    
-    @objc private func timeSetting() {
-        
-        let timeSettingviewController = TimeSettingViewController(isSelectedTime: false, delegate: self)
-        self.navigationController?.pushViewController(timeSettingviewController, animated: true)
-        
-    }
-    
     private lazy var tagButton = UIButton().then {
         $0.setTitle("Tag", for: .normal)
         $0.setTitleColor(.black, for: .normal)
@@ -57,6 +39,16 @@ final class MainViewController: UIViewController {
             action: #selector(openTagModal),
             for: .touchUpInside
         )
+    }
+
+    private lazy var countButton = UIButton(type: .roundedRect).then {
+        $0.setTitle("카운트 시작", for: .normal)
+        $0.addTarget(self, action: #selector(startTimer), for: .touchUpInside)
+    }
+
+    private lazy var timeButton = UIButton(type: .roundedRect).then {
+        $0.setTitle("시간 설정", for: .normal)
+        $0.addTarget(self, action: #selector(timeSetting), for: .touchUpInside)
     }
 
     override func viewDidLoad() {
@@ -78,7 +70,7 @@ final class MainViewController: UIViewController {
         updateTimeLabel()
         // FIXME: Remove startTimer() after implementing time setup
     }
-    
+
     private func updateTimeLabel() {
         let minutes = (maxTime - currentTime) / 60
         let seconds = (maxTime - currentTime) % 60
@@ -106,49 +98,16 @@ extension MainViewController {
         updateTimeLabel()
     }
 
-    @objc private func startTimer() {
-        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
-            let minutes = (self.maxTime - self.currentTime) / 60
-            let seconds = (self.maxTime - self.currentTime) % 60
-            
-            self.timeLabel.text = String(format: "%02d:%02d", minutes, seconds)
-            self.currentTime += 1
-
-            if self.currentTime > self.maxTime {
-                timer.invalidate()
-            }
-        }
-
-        private lazy var tagButton = UIButton().then {
-            $0.setTitle("Tag", for: .normal)
-            $0.setTitleColor(.black, for: .normal)
-            $0.titleLabel?.font = UIFont.systemFont(ofSize: 15)
-            $0.addTarget(
-                self,
-                action: #selector(openTagModal),
-                for: .touchUpInside
-            )
-        )
-
-        UNUserNotificationCenter.current()
-            .add(request) { error in
-                guard let error else { return }
-                print(error.localizedDescription)
-            }
+    @objc private func timeSetting() {
+        let timeSettingviewController = TimeSettingViewController(isSelectedTime: false, delegate: self)
+        navigationController?.pushViewController(timeSettingviewController, animated: true)
     }
 
-     @objc private func timeSetting() {
-
-         let timeSettingviewController = TimeSettingViewController(isSelectedTime: false, delegate: self)
-         self.navigationController?.pushViewController(timeSettingviewController, animated: true)
-
-     }
-    
     @objc private func startTimer() {
         timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
             let minutes = (self.maxTime - self.currentTime) / 60
             let seconds = (self.maxTime - self.currentTime) % 60
-            
+
             self.timeLabel.text = String(format: "%02d:%02d", minutes, seconds)
             self.currentTime += 1
 
@@ -175,11 +134,10 @@ extension MainViewController {
 
         UNUserNotificationCenter.current()
             .add(request) { error in
-                guard let error = error else { return }
+                guard let error else { return }
                 print(error.localizedDescription)
             }
     }
-
 }
 
 // MARK: - UI
