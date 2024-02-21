12475f25da181818092791d8d0c6068445ef03ea
haram22 <kimhr9889@handong.ac.kr>
2024-02-21 14:49:12 +0900

    [Fix] break time 연결

--- Sources/BreakScene/BreakTimerViewController.swift
+++ Sources/BreakScene/BreakTimerViewController.swift
@@ -14,7 +14,7 @@ final class BreakTimerViewController: UIViewController {
     private var timer: Timer?
     private var notificationId: String?
     private var currentTime = 0
-    private var maxTime = 0
+    private var maxTime = 25 * 60
     private var longPressTimer: Timer?
     private var longPressTime: Float = 0.0
 
@@ -67,7 +67,7 @@ final class BreakTimerViewController: UIViewController {
         view.backgroundColor = .white
         addSubviews()
         setupConstraints()
-
+        startTimer()
         longPressSetting(isEnable: false)
     }
 
