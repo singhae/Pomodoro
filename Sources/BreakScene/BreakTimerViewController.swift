8caf06e6e644f192435a9138f0211c2414486e82
haram22 <kimhr9889@handong.ac.kr>
2024-03-17 15:29:58 +0900

    fix
    [Fix] error 해결

--- Sources/BreakScene/BreakTimerViewController.swift
+++ Sources/BreakScene/BreakTimerViewController.swift
@@ -13,7 +13,7 @@ final class BreakTimerViewController: UIViewController {
     private var timer: Timer?
     private var notificationId: String?
     private var currentTime = 0
-    private var maxTime = 2 * 60
+    private var maxTime = 1 * 60
     private var longPressTimer: Timer?
     private var longPressTime: Float = 0.0
     private var timerHeightConstraint: Constraint?
