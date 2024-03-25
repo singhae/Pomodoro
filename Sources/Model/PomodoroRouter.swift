e8ece503ddc36bb466748df56584694cb0585dd7
jinsejin <j03298969@gmail.com>
2024-03-25 18:13:32 +0900

    [fix] pomodoroRouter 스텝시 나오는 네비게이션 부분 삭제

--- Sources/Model/PomodoroRouter.swift
+++ Sources/Model/PomodoroRouter.swift
@@ -55,6 +55,8 @@ final class PomodoroRouter {
     ) {
         let pomodoroTimerViewController = MainViewController()
         let breakTimerViewController = BreakTimerViewController()
+        pomodoroTimerViewController.navigationController?.isNavigationBarHidden = true
+        breakTimerViewController.navigationController?.isNavigationBarHidden = true
 
         switch currentStep {
         case .start:
