6f51bc928bbd05faba07485f6836707840b0c511
Seunghae Son <thsgo1357@naver.com>
2024-06-10 21:00:18 +0900

    [Bug][태그설정] 태그가 1개 남은 상태 > 마지막 태그 삭제 > 홈 이동 > 태그 설정 재진입 시 제거한 태그들이 살아나는 현상

--- Sources/MainScene/ViewControllers/TagModalViewController.swift
+++ Sources/MainScene/ViewControllers/TagModalViewController.swift
@@ -69,7 +69,7 @@ final class TagModalViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        defaultTagLoad()
+        loadDefaultTagsIfNeeded()
         setupViews()
         closeButton.addTarget(self, action: #selector(dismissModal), for: .touchUpInside)
         tagSettingCompletedButton.isEnabled = false // 첫 화면에는 설정완료 비활성화
@@ -135,24 +135,29 @@ final class TagModalViewController: UIViewController {
         }
     }
 
-    private func defaultTagLoad() {
-        guard let tagCount = try? RealmService.read(Tag.self).count, tagCount == 0 else { return }
+    private func loadDefaultTagsIfNeeded() {
+        if UserDefaults.standard.bool(forKey: "isFirstVisit") {
+            guard let tagCount = try? RealmService.read(Tag.self).count, tagCount == 0 else { return }
 
-        let defaultTags = [
-            Tag(tagName: "공부", colorIndex: "one", position: 0),
-            Tag(tagName: "수영", colorIndex: "two", position: 1),
-            Tag(tagName: "독서", colorIndex: "three", position: 2),
-        ]
+            let defaultTags = [
+                Tag(tagName: "공부", colorIndex: "one", position: 0),
+                Tag(tagName: "수영", colorIndex: "two", position: 1),
+                Tag(tagName: "독서", colorIndex: "three", position: 2),
+            ]
 
-        for tag in defaultTags {
-            do {
-                RealmService.write(tag)
-                Log.info("Added tag: \(tag.tagName)")
-            } catch {
-                Log.info("Error preloading tag \(tag.tagName): \(error)")
+            for tag in defaultTags {
+                do {
+                    RealmService.write(tag)
+                    Log.info("Added tag: \(tag.tagName)")
+                } catch {
+                    Log.info("Error preloading tag \(tag.tagName): \(error)")
+                }
             }
+
+            UserDefaults.standard.set(false, forKey: "isFirstVisit")
         }
     }
+
 
     private func addTagsToStackView() {
         for arrangedSubview in tagsStackView.arrangedSubviews {
